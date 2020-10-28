e925becd9f041b7d0c2869869971e33c58147416
BillyDevEnthusiast <68971587+BillyDevEnthusiast@users.noreply.github.com>
2020-10-28 15:33:28 +0200

    Exercise

---
+++ Python Fundamentals/Text Processing/01_valid_usernames.py
@@ -0,0 +1,14 @@
+user_names = input().split(", ")
+
+for username in user_names:
+    if not (3 <= len(username) <= 16):
+        continue
+    is_valid = True
+
+    for ch in username:
+        if not (ch.isalnum() or ch == "_" or ch == "-"):
+            is_valid = False
+            break
+    if not is_valid:
+        continue
+    print(username)
